bfff57cc2122e402862d662f2f318b4ba94e4bcc
nicopaez <nicopaez@gmail.com>
2014-05-05 10:39:28 -0400

    Build script updated

--- ci-build.sh
+++ ci-build.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 bundle exec rake test:tipit_extensions:unit
 rm -rf vendor/plugins/chiliproject_tipit_extensions # Remove this plugin because it modifies default behaviour and brake tests
 bundle exec rake test:$TEST_SUITE
