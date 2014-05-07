1c9629f9164731752b0ce60e70412725a8fed521
Nicolas Paez <nicopaez@gmail.com>
2014-05-07 12:33:52 -0400

    Update dbseed.sh

--- .cloud66/scripts/dbseed.sh
+++ .cloud66/scripts/dbseed.sh
@@ -2,6 +2,6 @@
 set -e
 cd $STACK_PATH
 sudo touch /tmp/hook_start
-bundle exec rake redmine:load_default_data
 bundle exec rake generate_session_store
+bundle exec rake redmine:load_default_data
 sudo touch /tmp/hook_end
