53f366cb88e2d89c8202cc187480e7012b7f17e7
NicoPaez <nicopaez@gmail.com>
2014-05-08 13:42:32 -0300

    Dependencies script updated

--- .cloud66/scripts/install_dependencies.sh
+++ .cloud66/scripts/install_dependencies.sh
@@ -2,8 +2,8 @@
 set -e
 
 # install postfix
-debconf-set-selections <<< "postfix postfix/mailname string $SERVER_NAME"
-debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+sudo debconf-set-selections <<< "postfix postfix/mailname string $SERVER_NAME"
+sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 sudo apt-get install -y postfix
 
 # configure posfix
