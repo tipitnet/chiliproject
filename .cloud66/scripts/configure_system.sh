c1e281582216d51653426c2e349d18698b1eefc9
NicoPaez <nicopaez@gmail.com>
2014-05-08 18:31:47 -0300

    Hook updated to grant file write permissions

--- .cloud66/scripts/configure_system.sh
+++ .cloud66/scripts/configure_system.sh
@@ -5,6 +5,12 @@ set -e
 cd $STACK_PATH 
 bundle exec rake redmine:load_default_data
 
+# configure file system write permissions
+cd $STACK_PATH 
+mkdir -p tmp public/plugin_assets
+sudo chown -R nginx:app_writers files log tmp public/plugin_assets
+sudo chmod -R 755 files log tmp public/plugin_assets
+
 # configure posfix
 alias=`cat $STACK_PATH/.cloud66/scripts/incoming_mail_configuration.txt`
 sudo eval echo "$alias" >> /etc/alias
