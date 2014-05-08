b885e3d43f886758e0d6dc11a753ac14845a829b
NicoPaez <nicopaez@gmail.com>
2014-05-08 13:26:37 -0300

    Postfix installation script added

--- .cloud66/scripts/install_dependencies.sh
+++ .cloud66/scripts/install_dependencies.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
+
+# install postfix
 debconf-set-selections <<< "postfix postfix/mailname string $SERVER_NAME"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 sudo apt-get install -y postfix
+
+# configure posfix
+alias=`cat $STACK_PATH/.cloud66/scripts/incoming_mail_configuration.txt`
+sudo eval echo "$alias" >> /etc/alias
+sudo newaliases
