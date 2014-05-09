ed6578defd05559b30efcfa7a229b73f9b1f9c8a
NicoPaez <nicopaez@gmail.com>
2014-05-09 14:03:10 -0300

    Configuration fixes

--- .cloud66/scripts/configure_system.sh
+++ .cloud66/scripts/configure_system.sh
@@ -10,8 +10,9 @@ cd $STACK_PATH
 mkdir -p tmp public/plugin_assets
 sudo chown -R nginx:app_writers files log tmp public/plugin_assets
 sudo chmod -R 755 files log tmp public/plugin_assets
+sudo chmod a+x extra/mail_handler/rdm-mailhandler.rb
 
 # configure posfix
 alias=`cat $STACK_PATH/.cloud66/scripts/incoming_mail_configuration.txt`
-eval echo "$alias" >> /etc/alias
+eval echo "$alias" >> /etc/aliases
 newaliases
--- .cloud66/scripts/install_dependencies.sh
+++ .cloud66/scripts/install_dependencies.sh
@@ -5,3 +5,7 @@ set -e
 sudo debconf-set-selections <<< "postfix postfix/mailname string $SERVER_NAME"
 sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 sudo apt-get install -y postfix
+
+#echo "${HOST_NAME}    ignored" >> /etc/postfix/virtual
+#echo "${INCOMING_EMAIL_ADDRESS}    chiliproject" >> /etc/postfix/virtual
+#postmap /etc/postfix/virtual
