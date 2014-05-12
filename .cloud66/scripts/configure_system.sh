71c7e3e2243ba998faf8e359c7a6ddfad9954714
NicoPaez <nicopaez@gmail.com>
2014-05-12 13:37:50 -0300

    configure script fixed

--- .cloud66/scripts/configure_system.sh
+++ .cloud66/scripts/configure_system.sh
@@ -22,17 +22,10 @@ newaliases
 
 sudo useradd -m -s /bin/bash chiliproject
 
-
-echo " alias_maps = hash:/etc/aliases
-alias_database = hash:/etc/aliases
-myorigin=kangaroo.chili-saas-singlebox-14.c66.me
-virtual_alias_domains = kangaroo.chili-saas-singlebox-14.c66.me
-virtual_alias_maps = hash:/etc/postfix/virtual
-mydestination = kangaroo.chili-saas-singlebox-14.c66.me, Kangaroo, chili-saas-singlebox-14kangaroo, localhost.localdomain, localhost
-
+cd /etc/postfix
 echo "virtual_alias_domains = ${SERVER_NAME}.${BOX_NAME}.c66.me" >> /etc/postfix/main.cf
 echo "virtual_alias_maps = hash:/etc/postfix/virtual" >> /etc/postfix/main.cf
-sed "s/mydestination =/mydestination = $SERVER_NAME.$BOX_NAME.c66.me/g" > new.main.cf
+sed "s/mydestination =/mydestination = $SERVER_NAME.$BOX_NAME.c66.me, /g" <main.cf >new.main.cf
 mv new.main.cf main.cf
 
 sudo postfix reload
