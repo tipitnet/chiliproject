8752c13e45774f4c5c544481a086aab7c118e3c5
NicoPaez <nicopaez@gmail.com>
2014-05-12 12:59:56 -0300

    Postfix config script updated

--- .cloud66/scripts/configure_system.sh
+++ .cloud66/scripts/configure_system.sh
@@ -16,8 +16,23 @@ sudo chmod a+x extra/mail_handler/rdm-mailhandler.rb
 export INCOMING_EMAIL_ADDRESS=chiliproject@${SERVER_NAME}.${BOX_NAME}.c66.me
 export SERVER_URL=http://${SERVER_NAME}.${BOX_NAME}.c66.me
 
-alias=`cat $STACK_PATH/.cloud66/scripts/incoming_mail_configuration.txt`
-eval echo "$alias" >> /etc/aliases
+alias=`cat $STACK_PATH/.cloud66/resources/aliases`
+echo "$alias" > /etc/aliases
 newaliases
 
 sudo useradd -m -s /bin/bash chiliproject
+
+
+echo " alias_maps = hash:/etc/aliases
+alias_database = hash:/etc/aliases
+myorigin=kangaroo.chili-saas-singlebox-14.c66.me
+virtual_alias_domains = kangaroo.chili-saas-singlebox-14.c66.me
+virtual_alias_maps = hash:/etc/postfix/virtual
+mydestination = kangaroo.chili-saas-singlebox-14.c66.me, Kangaroo, chili-saas-singlebox-14kangaroo, localhost.localdomain, localhost
+
+echo "virtual_alias_domains = ${SERVER_NAME}.${BOX_NAME}.c66.me" >> /etc/postfix/main.cf
+echo "virtual_alias_maps = hash:/etc/postfix/virtual" >> /etc/postfix/main.cf
+sed "s/mydestination =/mydestination = $SERVER_NAME.$BOX_NAME.c66.me/g" > new.main.cf
+mv new.main.cf main.cf
+
+sudo postfix reload
