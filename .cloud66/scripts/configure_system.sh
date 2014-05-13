9aaea88e982a057054383ff854eba0096f27f988
NicoPaez <nicopaez@gmail.com>
2014-05-13 19:07:24 -0300

    Configuration scripts updated

--- .cloud66/scripts/configure_system.sh
+++ .cloud66/scripts/configure_system.sh
@@ -17,7 +17,7 @@ cp $STACK_PATH/extra/mail_handler/* /etc/postfix/chili-handler
 sudo chmod a+x /etc/postfix/chili-handler/rdm-mailhandler.rb
 
 # configure posfix
-export INCOMING_EMAIL_ADDRESS=chiliproject@${SERVER_NAME}.${BOX_NAME}.c66.me
+export INCOMING_EMAIL_ADDRESS=chiliproject@${SERVER_NAME,,}.${BOX_NAME}.c66.me
 export SERVER_URL=http://${SERVER_NAME}.${BOX_NAME}.c66.me
 
 alias=`cat $STACK_PATH/.cloud66/resources/aliases`
@@ -27,9 +27,9 @@ newaliases
 sudo useradd -m -s /bin/bash chiliproject
 
 cd /etc/postfix
-echo "virtual_alias_domains = ${SERVER_NAME}.${BOX_NAME}.c66.me" >> /etc/postfix/main.cf
+echo "virtual_alias_domains = ${SERVER_NAME,,}.${BOX_NAME}.c66.me" >> /etc/postfix/main.cf
 echo "virtual_alias_maps = hash:/etc/postfix/virtual" >> /etc/postfix/main.cf
-sed "s/mydestination =/mydestination = $SERVER_NAME.$BOX_NAME.c66.me, /g" <main.cf >new.main.cf
+sed "s/mydestination =/mydestination = ${SERVER_NAME,,}.$BOX_NAME.c66.me, /g" <main.cf >new.main.cf
 mv new.main.cf main.cf
 
 sudo postfix reload
--- .cloud66/scripts/install_dependencies.sh
+++ .cloud66/scripts/install_dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-export HOST_NAME=${SERVER_NAME}.${BOX_NAME}.c66.me
-export INCOMING_EMAIL_ADDRESS=chiliproject@${SERVER_NAME}.${BOX_NAME}.c66.me
+export HOST_NAME=${SERVER_NAME,,}.${BOX_NAME}.c66.me
+export INCOMING_EMAIL_ADDRESS=chiliproject@${SERVER_NAME,,}.${BOX_NAME}.c66.me
 
 # install postfix
-sudo debconf-set-selections <<< "postfix postfix/mailname string $SERVER_NAME"
+sudo debconf-set-selections <<< "postfix postfix/mailname string ${SERVER_NAME,,}"
 sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 sudo apt-get install -y postfix
 
