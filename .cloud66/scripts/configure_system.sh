c6e96468ed3868594a5dc441d08ec80de5778b4b
Nicolas Paez <nicopaez@gmail.com>
2014-05-22 15:28:16 +0300

    Update configure_system.sh

--- .cloud66/scripts/configure_system.sh
+++ .cloud66/scripts/configure_system.sh
@@ -3,23 +3,23 @@ set -e
 
 # load default data
 cd $STACK_PATH 
-bundle exec rake db:migrate:plugins
-bundle exec rake redmine:load_default_data
+#bundle exec rake db:migrate:plugins
+#bundle exec rake redmine:load_default_data
 
 # configure file system write permissions
 cd $STACK_PATH 
-mkdir -p tmp public/plugin_assets
-sudo touch log/received_emails.log
-sudo chown -R nginx:app_writers files log tmp public/plugin_assets
-sudo chmod -R 755 files log tmp public/plugin_assets
+#mkdir -p tmp public/plugin_assets
+#sudo touch log/received_emails.log
+#sudo chown -R nginx:app_writers files log tmp public/plugin_assets
+#sudo chmod -R 755 files log tmp public/plugin_assets
 
 # configure mail handler scripts
-cd /etc/postfix
-sudo mkdir chili-handler
-cp $STACK_PATH/extra/mail_handler/* /etc/postfix/chili-handler 
-sudo chmod a+x /etc/postfix/chili-handler/rdm-mailhandler.rb
+#cd /etc/postfix
+#sudo mkdir chili-handler
+#cp $STACK_PATH/extra/mail_handler/* /etc/postfix/chili-handler 
+#sudo chmod a+x /etc/postfix/chili-handler/rdm-mailhandler.rb
 
 # create chiliproject user
-sudo useradd -m -s /bin/bash chiliproject
+#sudo useradd -m -s /bin/bash chiliproject
 
-./configure_postfix.sh
+$STACK_PATH/.cloud66/scripts/configure_postfix.sh
