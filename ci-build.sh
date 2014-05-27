69fe6d942b7a6315f14a29b1f06ce9d2e27d740a
NicoPaez <nicopaez@gmail.com>
2014-05-27 16:07:16 -0300

    Board watchers plugin added

--- ci-build.sh
+++ ci-build.sh
@@ -9,5 +9,6 @@ rm -rf vendor/plugins/chiliproject_tipit_extensions # Remove this plugin because
 rm -rf vendor/plugins/chiliproject_email_watchers # Remove this plugin because it modifies default behaviour and brake tests
 rm -rf vendor/plugins/chiliproject_enhanced_incoming_mail  # Remove this plugin because it modifies default behaviour and brake tests
 rm -rf vendor/plugins/chiliproject_clockability # Remove this plugin because it modifies default behaviour and brake tests
+rm -rf vendor/plugins/redmine_boards_watchers # Remove this plugin because it modifies default behaviour and brake tests
 
 bundle exec rake test:$TEST_SUITE
