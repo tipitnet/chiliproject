5faa811d4343effb275f16b9ea072b6c32edd0b7
NicoPaez <nicopaez@gmail.com>
2014-06-03 15:57:03 -0300

    Script fixed

--- .cloud66/scripts/post_deploy.sh
+++ .cloud66/scripts/post_deploy.sh
@@ -3,5 +3,5 @@ set -e
 
 cd $STACK_PATH 
 bundle exec rake generate_session_store
-sudo chown -R nginx:app_writers files log tmp public/plugin_assets
-sudo chmod -R 775 files log tmp public/plugin_assets	
+sudo chown -R nginx:app_writers files log tmp
+sudo chmod -R 775 files log tmp
