914bed5511dfd83d774bfd2f94684297b451b71e
NicoPaez <nicopaez@gmail.com>
2014-06-10 16:33:36 -0300

    Deploy script updated to relink files folder

--- .cloud66/scripts/post_deploy.sh
+++ .cloud66/scripts/post_deploy.sh
@@ -2,6 +2,8 @@
 set -e
 
 cd $STACK_PATH 
-bundle exec rake generate_session_store
+bundle exec rake db:migrate:plugins
+sudo rm -R files
+sudo ln -s /mnt/chiliproject1volume/tipitfiles/ files
 sudo chown -R nginx:app_writers files log tmp
 sudo chmod -R 775 files log tmp
