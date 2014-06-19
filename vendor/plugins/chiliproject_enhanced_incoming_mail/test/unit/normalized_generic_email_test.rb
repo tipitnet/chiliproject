e3ac888c05b51f502eeb0b530b43563307c75ce5
NicoPaez <nicopaez@gmail.com>
2014-06-19 10:40:02 -0300

    Test case added

--- vendor/plugins/chiliproject_enhanced_incoming_mail/test/unit/normalized_generic_email_test.rb
+++ vendor/plugins/chiliproject_enhanced_incoming_mail/test/unit/normalized_generic_email_test.rb
@@ -16,6 +16,24 @@ class NormalizedGenericEmailTest < ActiveSupport::TestCase
 		assert_match /First comment/, replaced_email
     assert_no_match /Priority: Normal/, replaced_email
 
-	end
+  end
+
+
+  def test_process_base64
+    # we are using thunderbird as generic
+    raw_email = get_mail_sample :mail_6708
+    email = Mail.new(raw_email.to_s)
+
+    normalized_email = EnhancedIncomingMail::NormalizedGenericEmail.new email
+
+    normalized_email.remove_nonhtml_text!()
+
+    normalized_email.remove_thread!
+
+    replaced_email = normalized_email.email
+    assert_match /I was able to get Translations working/, replaced_email
+    assert_no_match /Jimmy/, replaced_email
+
+  end
 
 end
