06ebea5242fd5cce15efffe6c3f78c68e082169b
NicoPaez <nicopaez@gmail.com>
2014-07-29 10:40:48 -0300

    Tipit Mail Handler plugin added

--- ci-build.sh
+++ ci-build.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 set -e
-bundle exec rake test:tipit_extensions:unit
-bundle exec rake test:enhanced_mail:unit
 bundle exec rake test:clockability:unit
 bundle exec rake test:clockability:functional
+bundle exec rake test:tipit_extensions:unit
+bundle exec rake test:tipit_mail_handler:unit
+bundle exec rake test:tipit_mail_handler:functional
 
 rm -rf vendor/plugins/chiliproject_tipit_extensions # Remove this plugin because it modifies default behaviour and brake tests
+rm -rf vendor/plugins/chiliproject_tipit_mail_handler # Remove this plugin because it modifies default behaviour and brake tests
 rm -rf vendor/plugins/chiliproject_email_watchers # Remove this plugin because it modifies default behaviour and brake tests
-rm -rf vendor/plugins/chiliproject_enhanced_incoming_mail  # Remove this plugin because it modifies default behaviour and brake tests
 rm -rf vendor/plugins/chiliproject_clockability # Remove this plugin because it modifies default behaviour and brake tests
 rm -rf vendor/plugins/chiliproject_etherpad # Remove this plugin because it could modify default behaviour and brake tests
 
