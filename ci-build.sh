42f73b7fa617390e5321a28de55537f120016a50
NicoPaez <nicopaez@gmail.com>
2014-08-01 19:43:10 -0300

    Test config updated

--- ci-build.sh
+++ ci-build.sh
@@ -7,7 +7,7 @@ bundle exec rake test:tipit_mailgun_handler:$TEST_SUITE
 rm -rf vendor/plugins/chiliproject_tipit_extensions # Remove this plugin because it modifies default behaviour and brake tests
 rm -rf vendor/plugins/chiliproject_tipit_mailgun_handler # Remove this plugin because it modifies default behaviour and brake tests
 rm -rf vendor/plugins/chiliproject_email_watchers # Remove this plugin because it modifies default behaviour and brake tests
-rm -rf vendor/plugins/chiliproject_clockability # Remove this plugin because it modifies default behaviour and brake tests
+rm -rf vendor/plugins/chiliproject_tipit_clockability # Remove this plugin because it modifies default behaviour and brake tests
 rm -rf vendor/plugins/chiliproject_etherpad # Remove this plugin because it could modify default behaviour and brake tests
 
 bundle exec rake test:$TEST_SUITE
