cdffacdde70ee37ff2a3a3f945508313f8dd57d7
NicoPaez <nicopaez@gmail.com>
2014-08-01 19:30:06 -0300

    Test configuration updated

--- ci-build.sh
+++ ci-build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 set -e
-bundle exec rake test:clockability:unit
-bundle exec rake test:clockability:functional
 bundle exec rake test:tipit_extensions:unit
+bundle exec rake test:clockability:$TEST_SUITE
 bundle exec rake test:tipit_mailgun_handler:$TEST_SUITE
 
 rm -rf vendor/plugins/chiliproject_tipit_extensions # Remove this plugin because it modifies default behaviour and brake tests
