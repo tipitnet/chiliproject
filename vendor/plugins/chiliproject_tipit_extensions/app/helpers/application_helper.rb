560bb69c4712cece587d3a26651ad4cf6b599ea7
NicoPaez <nicopaez@gmail.com>
2014-08-28 08:58:16 -0300

    Fix for issue 7211, img replaced for links in email notifications

--- vendor/plugins/chiliproject_tipit_extensions/app/helpers/application_helper.rb
+++ vendor/plugins/chiliproject_tipit_extensions/app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
     user.projects.collect{|proj| [ proj.name, proj.id]}.sort{|x,y| x.last <=> y.last }
   end
 
+  def replace_images_for_links(input)
+    @doc = Nokogiri::HTML(input.strip)
+    @doc.css('img').each do |node|
+      href = node.attr("src")
+      anchor = "[Inline Image: <a href=\"#{href}\">link</a> ]"
+      node.replace(anchor)
+    end
+    @doc.css('body').children.to_s
+  end
+
 end
---
+++ vendor/plugins/chiliproject_tipit_extensions/test/unit/application_helper_test.rb
@@ -0,0 +1,17 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+class ApplicationHelperTest < ActionView::TestCase
+
+  def test_replace_image_for_links
+    input = "<p><img src=\"http://localhost:3000/attachments/download/68\" alt=\"\" /></p>"
+    result = replace_images_for_links(input)
+    assert_equal("<p>[Inline Image: <a href=\"http://localhost:3000/attachments/download/68\">link</a> ]</p>", result)
+  end
+
+  def test_replace_image_for_links_not_affect_when_no_img
+    input = '<p>Hello world</p>'
+    result = replace_images_for_links(input)
+    assert_equal(input, result)
+  end
+
+end
