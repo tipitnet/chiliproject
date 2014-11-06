66e5ae16f9bce839173a6ab269c16149bb079442
NicoPaez <nicopaez@gmail.com>
2014-11-06 11:41:23 -0300

    Cloud66 files removed

--- .cloud66/scripts/post_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-cd $STACK_PATH 
-bundle exec rake generate_session_store
-sudo rm -R files
-sudo ln -s /mnt/chiliproject1volume/tipitfiles/ files
-sudo chown -R nginx:app_writers files log tmp
-sudo chmod -R 775 files log tmp
